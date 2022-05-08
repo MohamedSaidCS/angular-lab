709587ba23de437b2ee0db1a342792b7639f88ba
MohamedSaidCS <60986229+MohamedSaidCS@users.noreply.github.com>
2022-05-08 15:20:04 +0200

    lab 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import {StudentComponent} from "./student/student.component";
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    StudentComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
